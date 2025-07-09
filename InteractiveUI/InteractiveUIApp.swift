30b86d27a223f209b476e5e1dff10ec35ec6a635
Eunah <eunahmaumbe@gmail.com>
2025-07-09 14:33:24 -0500

    Initial Commit

---
+++ InteractiveUI/InteractiveUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIApp.swift
+//  InteractiveUI
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
